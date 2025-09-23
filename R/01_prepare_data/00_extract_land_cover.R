5ccef447d5dfe659d4fdc9b51b23b9ed767a8fa5
Selina Baldauf <selina.baldauf@gmx.de>
2025-09-23 16:40:12 +0200

    Update script - replace tidyverse with readr - make variable names more intuitive - remove unnecessary code

--- R/01_prepare_data/00_extract_land_cover.R
+++ R/01_prepare_data/00_extract_land_cover.R
@@ -1,13 +1,12 @@
-library(tidyverse)
+library(readr)
 library(terra)
 library(sf)
 library(exactextractr)
 
-hea <- read_csv("data-raw/headers.csv")
-hea |> str()
+headers <- read_csv("data-raw/headers.csv")
 
-# Spatial points
-hea_sf <- st_as_sf(hea, coords = c("lon", "lat"), crs = 4326)
+# Convert latitude and longitude to spatial points
+headers_sf <- st_as_sf(headers, coords = c("lon", "lat"), crs = 4326)
 
 # Raster data is downloaded from https://land.copernicus.eu/en/products/global-dynamic-land-cover/copernicus-global-land-service-land-cover-100m-collection-3-epoch-2019-globe
 # Loading raster data
@@ -23,23 +22,26 @@ crs_raster <- crs(builtup_rast)
 buffer_sizes <- c(250, 500, 1000, 2000)
 
 # spatial reprojection
-hea_proj <- st_transform(hea_sf, crs = crs_raster)
+headers_proj <- st_transform(headers_sf, crs = crs_raster)
 
-# Extracting values from rasters in the header
+# Extracting values from rasters in the headers
 
-result_hea <- hea
+result_headers <- headers
 
 for (buf in buffer_sizes) {
-  buffer <- st_buffer(hea_proj, dist = buf)
+  buffer <- st_buffer(headers_proj, dist = buf)
   builtup_mean <- exact_extract(builtup_rast, buffer, "mean")
   cropland_mean <- exact_extract(cropland_rast, buffer, "mean")
-  result_hea[[paste0("builtup_", buf, "m")]] <- builtup_mean
-  result_hea[[paste0("cropland_", buf, "m")]] <- cropland_mean
+  result_headers[[paste0("builtup_", buf, "m")]] <- builtup_mean
+  result_headers[[paste0("cropland_", buf, "m")]] <- cropland_mean
 }
 
-result_hea |> names()
-result_hea |>
+result_headers |> names()
+result_headers |>
   select(builtup_250m:cropland_2000m) |>
   str()
 
-write.csv(result_hea, "data/header_landcover_buffers.csv", row.names = FALSE)
+write_csv(
+  result_headers,
+  "data-raw/headers_landcover_buffers.csv"
+)
